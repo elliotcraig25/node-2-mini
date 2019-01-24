d0002bc00baf87f0a90415f6a5ae0e498617c1df
elliot <elliotgcraig@icloud.com>
2019-01-24 12:32:04 -0700

    finished mini project

---
+++ controller.js
@@ -0,0 +1,7 @@
+module.exports = {
+    getPlanes: (req, res, next)=>{
+        req.app.get('db').get_planes([25])
+        .then((planes)=>res.status(200).send(planes))
+        .catch((err)=>res.status(500).send(err))
+    }
+}
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+require('dotenv').config()
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const massive = require('massive');
+const ctrl = require('./controller')
 
 const app = express();
 app.use( bodyParser.json() );
 app.use( cors() );
+
+massive(process.env.CONNECTION_STRING).then((dbInstance)=>{
+    app.set('db', dbInstance)
+})
+
+app.get(`/api/planes`, ctrl.getPlanes)
 
 const port = process.env.PORT || 3000
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
